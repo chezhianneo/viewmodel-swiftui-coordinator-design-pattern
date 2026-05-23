66fed2f7b65d79c14e3fc9c173943d55ec297f08
Elan <neochezhian@gmail.com>
2026-05-23 16:40:35 -0700

    Initial Commit

---
+++ movie/movieApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieApp.swift
+//  movie
+//
+//  Created by Elan Arulraj on 5/23/26.
+//
+
+import SwiftUI
+
+@main
+struct movieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ movieTests/movieTests.swift
@@ -0,0 +1,19 @@
+//
+//  movieTests.swift
+//  movieTests
+//
+//  Created by Elan Arulraj on 5/23/26.
+//
+
+import Testing
+@testable import movie
+
+struct movieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
